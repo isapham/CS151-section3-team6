afd76fb3e60cfb2131977064da847cacf97987f5
jawnhoang <johnhoang5@gmail.com>
2020-12-03 17:28:04 -0800

    Revert "Delete Board.java"
    This reverts commit e3dcfeb2687426b13efe8d85e839d132e35bb70c.

---
+++ application/tetris/logic/Board.java
@@ -0,0 +1,25 @@
+package application.tetris.logic;
+
+public interface Board {
+	boolean moveBrickDown();
+
+    boolean moveBrickLeft();
+
+    boolean moveBrickRight();
+
+    boolean rotateLeftBrick();
+
+    boolean createNewBrick();
+
+    int[][] getBoardMatrix();
+
+    ViewData getViewData();
+
+    void mergeBrickToBackground();
+
+    ClearRow clearRows();
+
+    Score getScore();
+
+    void newGame();
+}
