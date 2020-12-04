96594276f8d3f97a325473929fc9a9e505f78de9
jawnhoang <54566354+jawnhoang@users.noreply.github.com>
2020-12-03 17:12:12 -0800

    updated javadoc for settingsController and a few other

---
+++ application/MainMenuController.java
@@ -0,0 +1,131 @@
+package application;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.stage.Stage;
+
+/**
+ * This class is for all the button functions that redirect to another pages
+ *
+ *
+ */
+public class MainMenuController implements Initializable{
+	@FXML  ImageView playIcon;
+	@FXML  ImageView snowFlakeIcon;
+	@FXML  ImageView houseIcon;
+	@FXML  ImageView championIcon;
+	
+	@Override
+	public void initialize (URL location, ResourceBundle resources) {
+		houseIcon.setImage(new Image("/main/resources/cs151GameShopIcon.png"));
+		playIcon.setImage(new Image("/main/resources/cs151GameConsoleIcon.png"));
+		snowFlakeIcon.setImage(new Image("/main/resources/cs151GameGearIconThing.png"));
+		championIcon.setImage(new Image("/main/resources/cs151GameHiScoreIcon.png"));
+	}
+	
+	/**
+	 * This method switches to games page 
+	 * 
+	 * @param even, button click
+	 * @throws IOException
+	 */
+	@FXML
+	public void buttonAction(ActionEvent event) throws IOException{
+		Stage currStage = (Stage) ((Node)event.getSource()).getScene().getWindow();
+        currStage.close();
+		FXMLLoader fxmlLoader = new FXMLLoader();
+		String pathToFxml = "src/main/resources/Games.fxml";
+		URL fxmlUrl = new File(pathToFxml).toURI().toURL();
+		fxmlLoader.setLocation(fxmlUrl);
+		Parent home_page_parent = fxmlLoader.load();
+		
+		Scene home_page_scene = new Scene(home_page_parent,844,750);
+		Stage scene2 = (Stage) ((Node)event.getSource()).getScene().getWindow();
+		scene2.setTitle("Games Home Page");
+		scene2.setScene(home_page_scene);
+		scene2.show();
+	}
+	
+	
+	/**
+	 * This method switches to Item Shop page
+	 * 
+	 * @param event
+	 * @throws IOException
+	 */
+	@FXML
+	public void switchToItemShop(ActionEvent event) throws IOException{
+		Stage currStage = (Stage) ((Node)event.getSource()).getScene().getWindow();
+        currStage.close();
+		FXMLLoader fxmlLoader = new FXMLLoader();
+		String pathToFxml = "src/main/resources/itemShop.fxml";
+		URL fxmlUrl = new File(pathToFxml).toURI().toURL();
+		fxmlLoader.setLocation(fxmlUrl);
+		Parent itemShopPage = fxmlLoader.load();
+		
+		Scene itemShopScene = new Scene(itemShopPage,844,750);
+		Stage itemShopStage = (Stage) ((Node)event.getSource()).getScene().getWindow();
+		itemShopStage.setScene(itemShopScene);
+		itemShopStage.setTitle("Item Shop");
+		itemShopStage.show();	
+	}
+	
+	/**
+	 * This method switches to Hi-score page
+	 * 
+	 * @param event, button click
+	 * @throws IOException
+	 */
+	@FXML
+	public void switchToHiScore(ActionEvent event) throws IOException{
+		Stage currStage = (Stage) ((Node)event.getSource()).getScene().getWindow();
+        currStage.close();
+		FXMLLoader fxmlLoader = new FXMLLoader();
+		String pathToFxml = "src/main/resources/HiScore.fxml";
+		URL fxmlUrl = new File(pathToFxml).toURI().toURL();
+		fxmlLoader.setLocation(fxmlUrl);
+		Parent home_page_parent = fxmlLoader.load();
+		
+		Scene home_page_scene = new Scene(home_page_parent,844,750);
+		Stage scene2 = (Stage) ((Node)event.getSource()).getScene().getWindow();
+		scene2.setScene(home_page_scene);
+		scene2.setTitle("Hi-Score Page");
+		scene2.show();
+	}
+	
+	/**
+	 * This method switches to settings page 
+	 * 
+	 * @param event, button click
+	 * @throws IOException
+	 */
+	@FXML
+	public void switchToGameSetting(ActionEvent event) throws IOException{
+		Stage currStage = (Stage) ((Node)event.getSource()).getScene().getWindow();
+        currStage.close();
+		FXMLLoader fxmlLoader = new FXMLLoader();
+		String pathToFxml = "src/main/resources/gameSettings.fxml";
+		URL fxmlUrl = new File(pathToFxml).toURI().toURL();
+		fxmlLoader.setLocation(fxmlUrl);
+		Parent settingPage = fxmlLoader.load();
+		
+		Scene setting_scene = new Scene(settingPage,844,750);
+		Stage scene2 = (Stage) ((Node)event.getSource()).getScene().getWindow();
+		scene2.setScene(setting_scene);
+		scene2.setTitle("Settings Page");
+		scene2.show();	
+	}
+	
+}
---
+++ application/pong/PongCircleController.java
@@ -0,0 +1,99 @@
+package application.pong;
+
+import java.util.Random;
+
+import application.GamesController;
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.image.Image;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.scene.text.TextAlignment;
+import javafx.util.Duration;
+
+public class PongCircleController extends PongController{
+	
+	public void initialize() {
+		ppoints=0;
+		playerPointIcon.setImage(new Image("/main/resources/cs151GameGalPointsIcon1.png"));
+		GraphicsContext gc = canvas.getGraphicsContext2D();
+		Timeline t1 = new Timeline(new KeyFrame(Duration.millis(10), e ->run(gc)));
+		t1.setCycleCount(Timeline.INDEFINITE);
+		t1.play();
+	}
+	private void run(GraphicsContext gc){
+        //set background color
+        gc.setFill(Color.BLACK);
+        gc.fillRect(0,0, GamesController.PONG_WIDTH , GamesController.PONG_HEIGHT);
+        //set text color
+        gc.setFill(Color.WHITE);
+        gc.setFont(Font.font(25));
+
+        if(GamesController.gameStarted){
+            //set ball movement
+        	ballXPos += ballXSpeed;
+            ballYPos += ballYSpeed;
+
+            //computer or CPU
+            if(ballXPos <  GamesController.PONG_WIDTH - GamesController.PONG_WIDTH/4){
+                GamesController.compPaddleYPos = ballYPos - GamesController.PADDLE_HEIGHT/2;
+            }
+            else{
+            	GamesController.compPaddleYPos =  ballYPos > GamesController.compPaddleYPos + GamesController.PADDLE_HEIGHT/2 ? GamesController.compPaddleYPos += 1: GamesController.compPaddleYPos - 1;
+            }
+
+            //draw ball
+            gc.fillOval(ballXPos, ballYPos, BALL_RADIUS, BALL_RADIUS);
+        }
+        else{
+            //set the start text
+            gc.setStroke(Color.WHITE);
+            gc.setTextAlign(TextAlignment.CENTER);
+            gc.strokeText("Click Enter to Start", GamesController.PONG_WIDTH/2, GamesController.PONG_HEIGHT/2 );
+
+            //reset ball start position
+            ballXPos = GamesController.PONG_WIDTH / 2;
+            ballYPos = GamesController.PONG_HEIGHT / 2;
+
+            //reset speed and direction
+            ballXSpeed = new Random().nextInt(2) == 0 ? 1: - 1;
+            ballYSpeed = new Random().nextInt(2) == 0 ? 1: - 1;
+
+        }
+
+        //ball stays in canvas
+        if(ballYPos > GamesController.PONG_HEIGHT || ballYPos < 0){
+            ballYSpeed *= -1;
+        }
+
+        //computer gets a point
+        if(ballXPos < playerPaddleXPos - GamesController.PADDLE_WIDTH){
+            compScore += 1;
+            GamesController.gameStarted = false;
+        }
+    	
+        //human player
+        if(ballXPos > compPaddleXPos + GamesController.PADDLE_WIDTH){
+        	pong_score(1);
+        	playerScore += 1;
+        	GamesController.gameStarted = false; 
+        }
+        
+        //increase ball speed
+        if(((ballXPos + BALL_RADIUS > compPaddleXPos) && ballYPos >= GamesController.compPaddleYPos && ballYPos <= GamesController.compPaddleYPos + GamesController.PADDLE_HEIGHT)
+                || ((ballXPos < playerPaddleXPos + GamesController.PADDLE_WIDTH) && ballYPos >= GamesController.playerPaddleYPos && ballYPos <= GamesController.playerPaddleYPos + GamesController.PADDLE_HEIGHT)){
+            ballYSpeed += 1 * Math.signum(ballYSpeed);
+            ballXSpeed += 1 * Math.signum(ballXSpeed);
+            ballXSpeed *= -1;
+            ballYSpeed *= -1;
+        }
+
+        //draw score
+        gc.fillText(playerScore + "\t\t\t\t\t\t\t" + compScore, GamesController.PONG_WIDTH/2, 100);
+
+        //draw player one and CPU
+        gc.fillRect(compPaddleXPos, GamesController.compPaddleYPos, GamesController.PADDLE_WIDTH, GamesController.PADDLE_HEIGHT);
+        gc.fillRect(playerPaddleXPos, GamesController.playerPaddleYPos, GamesController.PADDLE_WIDTH, GamesController.PADDLE_HEIGHT);
+    }
+}
